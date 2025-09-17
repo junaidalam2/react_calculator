a048e3f58bccbb25c8f8cc2c43762a36d6a69939
junaidalam2 <junaid.alam2@gmail.com>
2025-09-16 23:07:29 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+//import { useState } from 'react'
+//https://medium.com/@gosagnik/building-a-simple-calculator-with-react-js-65a5a2fb43e2
+
+import './App.css'
+
+
+// function MyButton({ buttonText }) {
+
+//   return (
+//     <button>{buttonText}</button>
+//   )
+// }
+
+
+function App() {
+
+  return (
+    <>
+      <button>0</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>+</button>
+      <button>-</button>
+      <button>x</button>
+      <button>÷</button>
+      <button>=</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+function Display({ value }) {
+    return (
+        <div className="display">
+            {value}
+        </div>
+    );
+}
+
+export default Display 
